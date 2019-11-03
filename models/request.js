9c784677cad290f5c2af670d376e7b19fc8fea0a
tempuser <kwekubondzie@gmail.com>
2019-11-03 01:02:50 -0500

    added a request

---
+++ models/request.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var mongoDb = 'mongodb://127.0.0.1/myDb'
+mongoose.connect(mongoDb, {useNewUrlParser: true} );
+var db = mongoose.connection;
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+//  _id: Number,
+  created_on: Date,
+  accepter: {type: Schema.Types.ObjectId, ref: 'User'},
+  thread: {type: Schema.Types.ObjectId, ref: 'Thread'},
+  stat: Number, // 0 for not yet 1 for acc -1 for no
+  requester: {type: Schema.Types.ObjectId, ref: 'User'},
+});
+module.exports = mongoose.model('User', UserSchema);
