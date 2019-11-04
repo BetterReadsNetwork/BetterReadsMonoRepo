521c1812f5375ab0607a34d55acc9b13f7a44db6
Alex Zhao <alextzhao1996@gmail.com>
2019-11-03 19:29:50 -0500

    adds comments for mongodb config file

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,6 @@
+// Created by Alex Zhao
+// see: https://medium.com/chingu/an-overview-of-mongodb-mongoose-b980858a8994
+
 let config = {};
 config.db = {};
 // Create properties on the config.db object for the host and
